e1edafd32beb9af77ed2f5de8bc0bfd98306b70a
Anas Asimi <anas2020cr7@gmail.com>
2024-09-03 19:38:11 +0100

    fix 1. FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -2,7 +2,8 @@
 """
 1-fifo_cache.py
 """
-import datetime
+
+from collections import OrderedDict
 from base_caching import BaseCaching
 
 
@@ -12,24 +13,21 @@ class FIFOCache(BaseCaching):
         BaseCaching (_type_): _description_
     """
 
+    def __init__(self):
+        """Initializes the cache.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
     def put(self, key, item):
         """ Add an item in the cache
         """
         if key is None or item is None:
             return None
-        self.cache_data[key] = {
-            'value': item,
-            'date': datetime.datetime.now()
-        }
+        self.cache_data[key] = item
         if len(self.cache_data) > self.MAX_ITEMS:
-            oldest_key = None
-            for key, value in self.cache_data.items():
-                if oldest_key is None:
-                    oldest_key = key
-                if self.cache_data[oldest_key]['date'] > value['date']:
-                    oldest_key = key
-            print("DISCARD:", oldest_key)
-            del self.cache_data[oldest_key]
+            first_key, value = self.cache_data.popitem(last=False)
+            print("DISCARD:", first_key)
 
     def get(self, key):
         """ Get an item by key
