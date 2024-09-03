c02e64f704019e9589512ae117b7ac476db1d632
Anas Asimi <anas2020cr7@gmail.com>
2024-09-03 18:54:22 +0100

    update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -36,10 +36,10 @@ class BaseCaching():
 
 
 class BasicCache(BaseCaching):
-    def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
+    """BasicCache
+    Args:
+        BaseCaching (_type_): _description_
+    """
 
     def put(self, key, item):
         """ Add an item in the cache
@@ -51,5 +51,4 @@ class BasicCache(BaseCaching):
     def get(self, key):
         """ Get an item by key
         """
-        if key in self.cache_data.keys():
-            return self.cache_data[key]
+        return self.cache_data.get(key, None)
