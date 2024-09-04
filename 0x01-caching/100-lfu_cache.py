67c575cb4dc753b8de09088a27c27dc8c2a30c26
Anas Asimi <anas2020cr7@gmail.com>
2024-09-04 01:45:10 +0100

    fix pycodestyle

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -33,12 +33,14 @@ class LFUCache(BaseCaching):
         if key is None or item is None:
             return None
 
-        if key not in self.cache_data and len(self.cache_data) == self.MAX_ITEMS:
+        if key not in self.cache_data \
+                and len(self.cache_data) == self.MAX_ITEMS:
             LENGTH = 10
             frequently_used_keys = [
                 k for k in self.get_history if k in self.cache_data]
 
-            if any(k not in frequently_used_keys for k in self.cache_data.keys()):
+            if any(k not in frequently_used_keys
+                   for k in self.cache_data.keys()):
                 for k in self.cache_data.keys():
                     if k not in frequently_used_keys:
                         self.cache_data.pop(k)
@@ -49,7 +51,8 @@ class LFUCache(BaseCaching):
                 lowest_number = list(frequently_used_keys_counted.values())[0]
 
                 for k in self.cache_data.keys():
-                    if frequently_used_keys_counted.get(k) == lowest_number or k not in frequently_used_keys:
+                    if frequently_used_keys_counted.get(k) == lowest_number \
+                            or k not in frequently_used_keys:
                         self.cache_data.pop(k)
                         print("DISCARD:", k)
                         break
