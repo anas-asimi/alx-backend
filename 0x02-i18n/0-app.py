6b9fcd1c8b62f8d3f5e7381e905957b63081bc21
Anas Asimi <anas2020cr7@gmail.com>
2024-09-11 01:03:32 +0100

    update task 0. Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hello_world() -> str:
+    """
+    hello_world
+    """
     return render_template('0-index.html')
 
 if __name__ == '__main__':
