e1efdb4a2a748975ab7d42d80e68d55a6689775f
Anas Asimi <anas2020cr7@gmail.com>
2024-09-14 00:49:03 +0100

    fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,12 +8,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_world() -> str:
     """
     hello_world
     """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
